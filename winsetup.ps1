4b8a372e212c034e6234282cbc2740befb527f39
Youssef Fjel <yousseffjelit@gmail.com>
2024-01-07 12:10:41 +0100

    update winsterup.ps1

--- winsetup.ps1
+++ winsetup.ps1
@@ -3,4 +3,4 @@ Remove-Item -Recurse -Force ~/AppData/Local/nvim
 Remove-Item -Recurse -Force ~/AppData/Local/nvim-data
 mkdir -p $HOME/.vim/undodir
 mkdir -p $HOME/.scripts
-New-Item -Path "$HOME\AppData\Local\nvim" -ItemType SymbolicLink -Target "$pwd\titus-kickstart"
+New-Item -Path "$HOME\AppData\Local\nvim" -ItemType SymbolicLink -Target "$pwd\nvim"
