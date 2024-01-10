73b62022b04828f75f572ee7a5c738542ef91b67
Youssef Fjel <yousseffjelit@gmail.com>
2024-01-10 16:17:36 +0000

    fix problem push

--- nvim/lua/vim-options.lua
+++ nvim/lua/vim-options.lua
@@ -1,7 +1,7 @@
 vim.cmd("set expandtab")
-vim.cmd("set tabstop=2")
-vim.cmd("set softtabstop=2")
-vim.cmd("set shiftwidth=2")
+vim.cmd("set tabstop=1")
+vim.cmd("set softtabstop=1")
+vim.cmd("set shiftwidth=1")
 vim.g.mapleader = " "
 
 -- Navigate vim panes better
