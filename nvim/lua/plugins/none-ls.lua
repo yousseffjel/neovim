6935c594b6fcafe4bcf0e803bd60775cbae6052d
YuSuFjel <yousseffjelit@gmail.com>
2024-06-01 00:36:05 +0100

    update

--- nvim/lua/plugins/none-ls.lua
+++ nvim/lua/plugins/none-ls.lua
@@ -9,6 +9,7 @@ return {
 				null_ls.builtins.diagnostics.erb_lint,
 				null_ls.builtins.diagnostics.rubocop,
 				null_ls.builtins.formatting.rubocop,
+    null_ls.builtins.diagnostics.djlint,
 			},
 		})
 
